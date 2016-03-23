9daa5f0f1786a66b4b24a6adba2c92820036e3b9
Steven Thompson <steven.thompson.ca@gmail.com>
2016-03-23 12:14:26 -0400

    Initial Commit

---
+++ Audio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Audio
+//
+//  Created by steve on 2016-03-23.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Audio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Audio
+//
+//  Created by steve on 2016-03-23.
+//  Copyright © 2016 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
